db3f34de6b73041f83924240a699d9d1d8512fda
Lucas Cardoso <mr.lucascardoso@gmail.com>
2016-04-04 00:16:07 -0300

    Primeira questão feira

--- src/com/questao1/Main.java
+++ src/com/questao1/Main.java
@@ -1,8 +1,51 @@
 package com.questao1;
 
-public class Main {
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.Arrays;
 
+public class Main {
+    /*
+    Fazer um programa que recebe 3 valores não inteiros do usuário
+    e mostra o maior deles, o menor deles e a média
+    */
     public static void main(String[] args) {
-        System.out.println("programa 1");
+        // Scanner para fazer a leitura do teclado
+        Scanner leitor = new Scanner(System.in);
+        // Array com os valores
+        float [] valores = new float[3];
+        // Boleando para fazer um controle, e finalizar somente se tudo feito corretamente
+        boolean concluido = false;
+        while (!concluido){
+            // Utilizando try-catch para ter certo controle de alguns erros
+            try{
+
+                //Digitando valores
+                for (int i = 0; i < valores.length; i++) {
+                    System.out.println("Digite o numero " + (i + 1) + ": ");
+                    valores[i] = leitor.nextFloat();
+                }
+
+                // Utilizando o metodo sort() da Classe Arrays para ordenar os
+                // valores, desta maneira pegando o menor e o maior valor
+                Arrays.sort(valores);
+
+                // Imprimindo o maior valor
+                System.out.println("O maior valor é: " + valores[2]);
+                // Imprimindo o menor valor
+                System.out.println("O menor valor é: " + valores[0]);
+                // Imprimindo a média dos valores
+                System.out.println("A média dos três valores é: " + (valores[0] + valores[1] + valores[2])/3);
+
+                // setando true para sair do while
+                concluido = true;
+            }catch (InputMismatchException ex){
+                // Prevendo que o usuario pode digitar algo que não seja um número, utilizei a Exception acima
+                // e reiniciando o leitor e o array
+                System.out.println("Digite somente números !\nReiniciando...\n");
+                valores = new float[3];
+                leitor = new Scanner(System.in);
+            }
+        }
     }
 }
