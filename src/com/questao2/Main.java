24a9ed21df62476316727c263ba5eea8c0a5f5f1
Lucas Cardoso <mr.lucascardoso@gmail.com>
2016-04-03 23:33:19 -0300

    commit inicial

---
+++ src/com/questao2/Main.java
@@ -0,0 +1,10 @@
+package com.questao2;
+
+/**
+ * Created by lucas on 03/04/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("programa 2");
+    }
+}
